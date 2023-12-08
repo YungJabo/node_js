1f4476244361a165b48990c953bcd1111253f6d3
Максим Кирсанов <kirsmaxi@gmail.com>
2023-12-08 14:58:23 +0300

    Исправлено 3е ДЗ

--- 3/routes/admin.js
+++ 3/routes/admin.js
@@ -6,81 +6,70 @@ const formidable = require('formidable')
 const db = require('../model')
 
 router.get('/', (req, res, next) => {
-  // TODO: Реализовать, подстановку в поля ввода формы 'Счетчики'
-  // актуальных значений из сохраненых (по желанию)
-  res.render('pages/admin', { title: 'Admin page' })
+  res.render('pages/admin', {
+    title: 'Admin page',
+    msgskill: req.flash('message')[0],
+    msgfile: req.flash('message2')[0],
+  })
 })
 
 router.post('/skills', (req, res, next) => {
-  /*
-  TODO: Реализовать сохранение нового объекта со значениями блока скиллов
-
-    в переменной age - Возраст начала занятий на скрипке
-    в переменной concerts - Концертов отыграл
-    в переменной cities - Максимальное число городов в туре
-    в переменной years - Лет на сцене в качестве скрипача
-  */
   const formData = req.body
-  const filePath = path.join(__dirname, '..', 'data.json')
-  const data = JSON.parse(fs.readFileSync(filePath))
-  const newData = [
-    {
-      number: formData.age,
-      text: 'Возраст начала занятий на скрипке',
-    },
-    {
-      number: formData.concerts,
-      text: 'Концертов отыграл',
-    },
-    {
-      number: formData.cities,
-      text: 'Максимальное число городов в туре',
-    },
-    {
-      number: formData.years,
-      text: 'Лет на сцене в качестве скрипача',
-    },
-  ]
-  // data.skills = newData
-  // const updatedData = JSON.stringify(data, null, 2)
-  // fs.writeFileSync(filePath, updatedData)
-  db.set('skills', newData).write()
-  // res.send('Реализовать сохранение нового объекта со значениями блока скиллов')
+  let validation = true
+  Object.keys(formData).forEach((key) => {
+    if (!formData[key]) {
+      validation = false
+    }
+  })
+  if (validation) {
+    Object.keys(formData).forEach((key) => {
+      db.get('skills')
+        .find({ type: key })
+        .assign({ number: formData[key] })
+        .write()
+    })
+    req.flash('message', 'Данные успешно отправлены!')
+  } else {
+    req.flash('message', 'Введие корректные данные!')
+  }
+
+  res.redirect('/admin')
 })
 
 router.post('/upload', (req, res, next) => {
-  /* TODO:
-   Реализовать сохранения объекта товара на стороне сервера с картинкой товара и описанием
-    в переменной photo - Картинка товара
-    в переменной name - Название товара
-    в переменной price - Цена товара
-    На текущий момент эта информация хранится в файле data.json  в массиве products
-  */
   const dbPath = path.join(__dirname, '..', 'data.json')
   const dbData = JSON.parse(fs.readFileSync(dbPath))
   const formData = new formidable.IncomingForm()
   const filesFolder = path.join(__dirname, '..', 'upload')
+  let validation = true
   formData.parse(req, function (err, fields, files) {
-    console.log(files.photo[0], fields)
     if (err) {
       return next(err)
     }
-    const fileName = path.join(filesFolder, files.photo[0].originalFilename)
-    fs.rename(files.photo[0].filepath, fileName, function (err) {
-      if (err) {
-        console.log(err)
-        return
-      }
-    })
-    const newData = {
-      src: './' + files.photo[0].originalFilename,
-      name: fields.name[0],
-      price: Number(fields.price[0]),
+    if (!fields.name[0] || !fields.price[0] || !files) {
+      validation = false
     }
-    // dbData.products.push(newData)
-    // const updatedData = JSON.stringify(dbData, null, 2)
-    // fs.writeFileSync(dbPath, updatedData)
-    db.get('products').push(newData).write()
+    const fileName = path.join(filesFolder, files.photo[0].originalFilename)
+
+    if (validation) {
+      fs.rename(files.photo[0].filepath, fileName, function (err) {
+        if (err) {
+          console.log(err)
+          return
+        }
+      })
+      const newData = {
+        src: './' + files.photo[0].originalFilename,
+        name: fields.name[0],
+        price: Number(fields.price[0]),
+      }
+
+      db.get('products').push(newData).write()
+      req.flash('message2', 'Данные успешно отправлены!')
+    } else {
+      req.flash('message2', 'Введите корректные данные!')
+    }
+    res.redirect('/admin')
   })
 })
 
--- 3/routes/login.js
+++ 3/routes/login.js
@@ -3,18 +3,14 @@ const router = express.Router()
 const fs = require('fs')
 const path = require('path')
 
-// const isAdmin = (req, res, next) => {
-//   if (req.session.isAdmin) {
-//     return next()
-//   }
-// }
 router.get('/', (req, res, next) => {
-  res.render('pages/login', { title: 'SigIn page' })
+  res.render('pages/login', {
+    title: 'SigIn page',
+    msglogin: req.flash('messageLogin')[0],
+  })
 })
 
 router.post('/', (req, res, next) => {
-  // TODO: Реализовать функцию входа в админ панель по email и паролю
-
   const formData = req.body
   const filePath = path.join(__dirname, '..', 'data.json')
   const data = JSON.parse(fs.readFileSync(filePath))
@@ -24,8 +20,9 @@ router.post('/', (req, res, next) => {
   ) {
     res.render('pages/admin', { title: 'Admin page' })
   } else {
-    res.send('Ошибка! Неверно введены данные!')
+    req.flash('messageLogin', 'Ошибка! Введенные данные не совпадают!')
   }
+  res.redirect('/login')
 })
 
 module.exports = router
